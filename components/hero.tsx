89264e2741b06a959eac3f584e45c1a96f79088d
Dearz-MD_Official <dearlyfebriano08@gmail.com>
2023-10-13 20:08:03 +0700

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -25,7 +25,7 @@ export default function Hero() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h1 mb-4" data-aos="fade-up">Company Profile Team RedaX</h1>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Selamat pagi, yang terhormat para hadirin, saya sangat senang bisa berbicara kepada Anda hari ini tentang perusahaan luar biasa yang menjadi tempat berkumpulnya para tim IT berbakat. Mari kita eksplorasi bersama apa yang membuat perusahaan ini begitu istimewa.</p>
+            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Hai User / Client, yang terhormat para hadirin, saya sangat senang bisa berbicara kepada Anda hari ini tentang perusahaan luar biasa yang menjadi tempat berkumpulnya para tim IT berbakat. Mari kita eksplorasi bersama apa yang membuat perusahaan ini begitu istimewa.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
                 <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Eksplor Information Technology</a>
